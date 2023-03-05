eba51041580473b81e6f4b07a2245e4eec0c1dc7
saransh-khobragade <saransh98@gmail.com>
2023-03-05 12:16:04 +0530

    pico added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,50 @@ export default function Home() {
 
   return (
     <div>
-      Hello Home
+     <main className="container">
+        <figure>
+          <nav>
+            <ul>
+              <li><a href="/"><strong>InterviewBro1</strong></a></li>
+            </ul>
+            <ul>
+              <li><a href="#">Javascript</a></li>
+              <li><a href="#">Golang</a></li>
+              <li><a href="#">Datastructures</a></li>
+              <li><a href="#">SystemDesign</a></li>
+              <li><a href="#">Projects</a></li>
+            </ul>
+          </nav>
+        </figure>
+
+
+        <main className="container">
+          <input style={{ "height": "10%", width: "100%" }} type="text" id="firstname" name="firstname" placeholder="Search" required />
+        </main>
+
+        <main className="container">
+          <details>
+            <summary>Question1</summary>
+            <p>Answer</p>
+          </details>
+
+          <details open>
+            <summary>Question2</summary>
+            <ul>
+              <li>Answer</li>
+              <li>
+                <pre>
+                <code>
+                  fetch("http://example.com/movies.json").then((response) => response.json()).then((data) => console.log(data))
+                </code>
+              </pre>
+
+              </li>
+            </ul>
+          </details>
+        </main>
+      </main>
+
     </div>
   );
 }
