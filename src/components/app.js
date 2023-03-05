090b7207d94bdcbe46621cfddfa0e91a99aa36c2
saransh-khobragade <saransh98@gmail.com>
2023-03-05 11:19:36 +0530

    done

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-
 import Home from './home';
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-    },
-    {
-        path: "home",
-        element: <Home/>,
-    },
-]);
 
 const App = (props) => {
     return (
-        <RouterProvider router={router} />
+        <Home />
     )
 }
 export default App;
